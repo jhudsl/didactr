fc3e25e06759081f5ae91a5dce053bc05b2939e2
muschellij2 <muschellij2@gmail.com>
2018-07-02 14:12:05 -0400

    Removing example.pdf again.

--- tests/testthat/test-pdf_to_images.R
+++ tests/testthat/test-pdf_to_images.R
@@ -4,7 +4,7 @@ test_that("PDF converted to PNG", {
 
   skip_on_cran()
 
-  ex_file = "example.pdf"
+  ex_file = system.file("extdata", "example.pdf", package = "didactr")
   res = pdf_to_images(ex_file)
 
   expect_length(res, 2)
